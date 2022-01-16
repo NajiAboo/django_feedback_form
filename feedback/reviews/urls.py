e469831ce6fe3fd55a48481b6453c3d6492bbd88
t0393 <naji.aboo@triassicsolutions.com>
2022-01-16 08:48:37 +0530

    Added the forms with the django

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .  import views
 urlpatterns = [
-    path("",views.review)
+    path("",views.review),
+    path("thank-you", views.thank_you)
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
+from .forms import ReviewForm
 # Create your views here.
 
 def review(request):
-    pass
+    if request.method == "POST":
+       form = ReviewForm(request.POST)
+       
+       if form.is_valid():
+           print(form.cleaned_data)        
+           return HttpResponseRedirect("/thank-you")
+       
+    else:
+        form = ReviewForm()
+    
+    return render(request,"reviews/review.html",{
+            "form":form
+        })   
+    
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
